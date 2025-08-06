1719aba2f78b718626194a0091ef92a01111b0d1
ju3tin <ju3tin@hotmail.co.uk>
2025-08-06 20:44:28 +0100

    dfg

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -7,7 +7,7 @@ import Game from '../../components/Game2';
 import Game5 from '../../components/Game6';
 import CrashList from '../../components/CrashList2';
 import GameControls from '../../components/GameControls2';
-import BetList from '../../../components/BetList2';
+import BetList from '../../components/BetList2';
 import axiosInstance from '../../../../lib/axiosInstance';
 
 import GameLayout from '../../components/GameLayout3';
