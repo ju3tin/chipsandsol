8a3bafb53eedf95088bc64d39204b2a176980cc3
ju3tin <ju3tin@hotmail.co.uk>
2025-08-06 17:47:47 +0100

    sdf

--- store/gameStore1.ts
+++ store/gameStore1.ts
@@ -13,10 +13,10 @@ import { create } from "zustand";
 
 //import Cors from "cors";
 
-import { elapsedToMultiplier } from '../../lib/utils';
+import { elapsedToMultiplier } from '../lib/utils';
 import { Wallet } from "lucide-react";
 
-import { address1a } from "../../src/components/WalletConnection";
+import { address1a } from "../components/WalletConnection";
 //export let address1a: string = '';
 //const { wallet1a } = useWalletContext();
 export type GameStatus =
