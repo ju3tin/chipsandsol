e76c4cfe79ab3e63e704276d2b11472688396a5f
ju3tin <ju3tin@hotmail.co.uk>
2025-08-06 17:12:03 +0100

    sdf

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import SideNav from '../components/side-nav';
 import Head from 'next/head'
 import { Analytics } from "@vercel/analytics/react";
 import Header from './header';
