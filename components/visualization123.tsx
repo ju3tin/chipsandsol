03afb214ade36abbbe8abe569bdcde81f1cdf6c0
ju3tin <ju3tin@hotmail.co.uk>
2025-09-30 12:21:16 +0100

    updateed the image

--- components/visualization123.tsx
+++ components/visualization123.tsx
@@ -397,7 +397,7 @@ const GameVisual: React.FC<GameVisualProps> = ({
         <>
           <span
             style={{
-              top: '137.5px',
+              top: '68.75px',
               left: "50%",
               transform: "translateX(-50%)",
               display: "block",
