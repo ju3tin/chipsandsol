dca7cc0ea0be30522b70412077c8479485dee165
ju3tin <ju3tin@hotmail.co.uk>
2025-10-01 14:02:49 +0100

    sdfdsfger

--- components/ylabels.tsx
+++ components/ylabels.tsx
@@ -28,16 +28,16 @@ export default function YLabels({ currentMultiplier, timer5 }: Props) {
       {
   currentMultiplier > 1.00 && currentMultiplier < 2.00 && (
   <ul style={{listStyle: "none", padding: 0, margin: 0, display: 'inline-block', position: 'absolute', zIndex: 11,}}>
-    <li style={{marginRight: "10px"}}>Item 1</li>
-    <li style={{marginRight: "10px"}}>Item 2</li>
-    <li style={{marginRight: "10px"}}>Item 3</li>
-    <li style={{marginRight: "10px"}}>Item 4</li>
-    <li style={{marginRight: "10px"}}>Item 5</li>
-    <li style={{marginRight: "10px"}}>Item 6</li>
-    <li style={{marginRight: "10px"}}>Item 7</li>
-    <li style={{marginRight: "10px"}}>Item 8</li>
-    <li style={{marginRight: "10px"}}>Item 9</li>
-    <li style={{marginRight: "10px"}}>Item 10</li>
+    <li style={{marginRight: "5px"}}>Item 1</li>
+    <li style={{marginRight: "5px"}}>Item 2</li>
+    <li style={{marginRight: "5px"}}>Item 3</li>
+    <li style={{marginRight: "5px"}}>Item 4</li>
+    <li style={{marginRight: "5px"}}>Item 5</li>
+    <li style={{marginRight: "5px"}}>Item 6</li>
+    <li style={{marginRight: "5px"}}>Item 7</li>
+    <li style={{marginRight: "5px"}}>Item 8</li>
+    <li style={{marginRight: "5px"}}>Item 9</li>
+    <li style={{marginRight: "5px"}}>Item 10</li>
   </ul>
 
 )
