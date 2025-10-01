fcb5d1dbd24ce329c8b7cbbf71c5ec95f41793b4
ju3tin <ju3tin@hotmail.co.uk>
2025-10-01 17:04:02 +0100

    sdf

--- components/curveupdate.tsx
+++ components/curveupdate.tsx
@@ -86,7 +86,7 @@ const BezierCanvasPage = () => {
   }, []);
 
   return (
-    <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
+    <div style={{ position: 'absolute', top: 0, left: 0, }}>
       <canvas
         ref={canvasRef}
         width={400}
