604214aa7e17540a4458a36225ffc20ce884b3cc
ju3tin <ju3tin@hotmail.co.uk>
2025-10-05 10:42:29 +0100

    dsfsfw

--- components/ylabels.tsx
+++ components/ylabels.tsx
@@ -6,13 +6,9 @@ type Props = {
   export default function YLabels({ currentMultiplier, timer5 }: Props) {
     // Define arrays for each multiplier range with a time property (in seconds)
     const ranges = [
-      { min: 1.0, max: 2.0, values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], time: 5 },
-      { min: 2.01, max: 4.0, values: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20], time: 4 },
-      { min: 4.01, max: 8.0, values: [3, 6, 9, 12, 15, 18, 21, 24, 27, 30], time: 3 },
-      { min: 8.01, max: 16.0, values: [4, 8, 12, 16, 20, 24, 28, 32, 36, 40], time: 2.5 },
-      { min: 16.01, max: 32.0, values: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50], time: 2 },
-      { min: 32.01, max: 64.0, values: [6, 12, 18, 24, 30, 36, 42, 48, 54, 60], time: 1.5 },
-      { min: 64.01, max: 128.0, values: [7, 14, 21, 28, 35, 42, 49, 56, 63, 70], time: 1 },
+      { min: 1.0, max: 2.60, values: [2, 4, 6, 8, 10, 12], time: 10 },
+      { min: 2.61, max: 100.0, values: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100], time: 70 },
+     
     ];
   
     // Find the matching range for currentMultiplier
