a723775c294f4794b13676227a9ddce395f7918a
ju3tin <ju3tin@hotmail.co.uk>
2025-10-06 08:05:34 +0100

    sdf

--- components/visualization123.tsx
+++ components/visualization123.tsx
@@ -478,10 +478,10 @@ const GameVisual: React.FC<GameVisualProps> = ({
    // height: "100vh", // Full viewport height
     //width: "100vw",  // Full viewport width
     position: "absolute",
-    top: "50%",
+   // top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    zIndex: 1000,
+  //  zIndex: 1000,
   }}
 >
   <Image
