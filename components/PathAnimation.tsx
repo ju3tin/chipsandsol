b1f80ea1859d0c92c6c885a887a8f5aab4097899
ju3tin <ju3tin@hotmail.co.uk>
2025-10-06 23:04:39 +0100

    sdf

--- components/PathAnimation.tsx
+++ components/PathAnimation.tsx
@@ -161,6 +161,7 @@ const BezierAnimation: React.FC<GameVisualProps> = ({
   }, []);
 
   return (
+    <div className="relative h-64 bg-gray-900 overflow-hidden mb-4">
     <div className="absolute inset-0">
     <canvas
       ref={canvasRef}
@@ -171,6 +172,7 @@ const BezierAnimation: React.FC<GameVisualProps> = ({
         zIndex: 100,
       }}
     />
+    </div>
     </div>
   );
 };
