966140bad34c5097132d822d9bec5c58b3458e4d
ju3tin <ju3tin@hotmail.co.uk>
2025-10-13 14:17:01 +0100

    sdf

--- components/labels12a.tsx
+++ components/labels12a.tsx
@@ -202,7 +202,7 @@ export default function BezierCurve() { // Defines a React functional component
                     fill: #333;
                 }
             `}</style> {/* Closes inline styles */}
-            <svg width="400" height="200" ref={svgRef}  className="w-full h-full" // Makes canvas fill its container
+            <svg width={400} height={200} ref={svgRef}  className="w-full h-full" // Makes canvas fill its container
             style={{
               zIndex: 101, // Ensures canvas is above other elements
             }}></svg> {/* SVG with typed ref */}
