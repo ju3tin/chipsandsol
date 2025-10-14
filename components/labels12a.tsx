ae3854963187d1931cd2d7d40cdacc3235396725
ju3tin <ju3tin@hotmail.co.uk>
2025-10-14 09:26:36 +0100

    sdf

--- components/labels12a.tsx
+++ components/labels12a.tsx
@@ -53,7 +53,7 @@ export default function BezierCurve() { // Defines a React functional component
 
         // Initial Axes
         xAxisGroup.call(d3.axisBottom(x).tickValues([0, 2, 4, 6, 8]).tickFormat(d3.format('d'))); // Renders x-axis with ticks 0-8
-        yAxisGroup.call(d3.axisLeft(y).tickValues(d3.range(0, 6)).tickFormat(d3.format('d'))); // Renders y-axis with ticks 0-5
+        yAxisGroup.call(d3.axisLeft(y).tickValues([2]).tickFormat(d3.format('d'))); // Renders y-axis with ticks 0-5
 
       //  xAxisGroup.call(d3.axisBottom(x).tickValues(d3.range(0, 9))); // Renders x-axis with ticks 0-8
       //  yAxisGroup.call(d3.axisLeft(y).tickValues(d3.range(0, 6))); // Renders y-axis with ticks 0-5
